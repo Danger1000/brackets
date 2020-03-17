b319de0ce234dddc41fcc015372c8b149cf7cea4
Danger1000 <60090781+Danger1000@users.noreply.github.com>
2020-03-17 05:59:48 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  if (str.length % 2 != 0) return false;
+  let openers = [],
+      closers = [];
+  for (let i=0; i<config.length; i++) {
+    openers.push(config[i][0]);
+    closers.push(config[i][1]);
+  } 
+    let matchOpen,
+      BracketsArray = [];
+  for (let i = 0; i < str.length; i++) {
+    let bracket = str[i];
+    for (let j = 0; j < openers.length; j++) {
+      if (bracket == openers[j] && bracket == closers[j]) {
+        if (BracketsArray.indexOf(bracket) == -1) {
+          BracketsArray.push(bracket);
+        } else {
+          matchOpen = openers[closers.indexOf(bracket)];
+          if (BracketsArray.pop() != matchOpen) {
+            return false;
+          }
+        }
+      } else {
+        if (bracket == openers[j]) {
+          BracketsArray.push(bracket);
+        }
+        if (bracket == closers[j]) {
+          matchOpen = openers[closers.indexOf(bracket)];
+          if (BracketsArray.pop() != matchOpen) {
+            return false;
+          }
+        }
+      }
+    }
+  }
+  return BracketsArray.length == 0 ? true : false;
 }
